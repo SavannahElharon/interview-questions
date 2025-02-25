226111815d20be29535875a7e207958f9bbde565
SavannahElharon <sae65@drexel.edu>
2025-02-25 00:38:07 -0500

    Update lru.py

--- python/src/lrucache/lru.py
+++ python/src/lrucache/lru.py
@@ -5,11 +5,18 @@ class LRUCache:
         self.lru = {}
 
     def refer(self, key):
-        # not present in cache
-
-        # present in cache
+         # not present in cache
+        if key not in self.lru:
+            if len(self.cache) == self.capacity:
+                last = self.cache.pop(key)
+                del self.lru[key]
+         # present in cache
+        else:
+            self.cache.remove(key)
 
         # update reference
+        self.cache.append(key)
+        self.lru[key] = True
 
     def display(self):
         for i in self.cache:
